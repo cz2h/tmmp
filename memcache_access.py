9c63a1ff971c5f1a37679f5e93cfa81c09395899
chuhao zeng <chuhaozeng@chuhaodeMacBook-Pro.local>
2022-03-23 20:16:17 -0400

    init

---
+++ memcache_access.py
@@ -0,0 +1,216 @@
+import random
+from sys import getsizeof
+from app import instance_id, memcache, memcache_stat, memcache_config, client
+from app.db_access import connect_to_database
+from datetime import datetime
+
+
+# Random Replacement Policy
+def random_replace_memcache():
+    # Check if memcache is empty
+    if bool(memcache):
+        # Randomly chose one key to drop from memcache
+        rand_key = random.choice(list(memcache.keys()))
+        memcache_stat['size'] -= memcache[rand_key]['size']
+        memcache.pop(rand_key)
+        return True
+    else:
+        print("Error in replacement, can't pop anymore because memcache is already empty. ")
+        return False
+
+
+# LRU Replacement Policy
+def lru_replace_memcache():
+    # Check if memcache is empty
+    if bool(memcache):
+        # Get the LRU timestamp
+        oldest_timestamp = min([d['timestamp'] for d in memcache.values()])
+        # Find the key by value
+        for mem_key in memcache.keys():
+            if memcache[mem_key]['timestamp'] == oldest_timestamp:
+                memcache_stat['size'] -= memcache[mem_key]['size']
+                memcache.pop(mem_key)
+                return True
+    else:
+        print("Error in replacement, can't pop anymore because memcache is already empty. ")
+        return False
+
+
+def replace_memcache():
+    """Execute a replacement policy specified by memcache_config['rep_policy']
+        This function will only pop from memcache, not the actual replacement.
+        New entry will be added by whoever called this function.
+    """
+    succeed = False
+    if memcache_config['rep_policy'] == 'RANDOM':
+        succeed = random_replace_memcache()
+    elif memcache_config['rep_policy'] == 'LRU':
+        succeed = lru_replace_memcache()
+    # make sure no error happened during pop
+    if succeed is True:
+        memcache_stat['num'] -= 1
+
+
+def update_memcache_stat(missed):
+    """Keep in mind this function does NOT update 'num' or 'size' of memcache
+        which makes it usable for missed situations
+
+    :param missed: bool
+    :return: None
+    """
+    if missed:
+        memcache_stat['mis'] += 1
+    else:
+        memcache_stat['hit'] += 1
+    memcache_stat['total'] += 1
+    memcache_stat['mis_rate'] = memcache_stat['mis'] / memcache_stat['total']
+    memcache_stat['hit_rate'] = memcache_stat['hit'] / memcache_stat['total']
+
+
+def add_memcache(key, file):
+    """Update the memcache and related statistic, request access to database when a miss happened
+
+    :param key: str
+    :param file: str
+    :return: None
+    """
+    # converts the image size to MB. There is 49 Bytes of overhead in string object
+    image_size = (getsizeof(file) - 49) / 1024 / 1024
+    # check if image can be fit into the memcache before everything else
+    if image_size > memcache_config['capacity']:
+        print('The given file is exceeded the capacity of the memcache')
+        return False
+
+    # If the key existed in Memcache, we need to update the size by subtracting by the old file size
+    if key in memcache.keys():
+        old_file_size = memcache[key]['size']
+        if old_file_size is None:   # memcache & DB inconsistency, found in memcache but not in DB
+            print("Returning in add_memcache, old file not found. Key in memcache = %d")
+            return False
+        # Update memcache statistic('num' in cache is not changing yet b/c we don't need to pop that entry)
+        memcache_stat['size'] -= old_file_size
+
+    # Store the file into memcache(replace other cache files if needed)
+    # Keep popping until we have enough space in memcache for the new image
+    while memcache_stat['size'] + image_size > memcache_config['capacity']:
+        replace_memcache()   # stats are updated inside
+
+    if key in memcache.keys():
+        memcache[key]['file'] = file            # update/add it to memcache
+        memcache[key]['size'] = image_size      # update the new file size
+        memcache[key]['timestamp'] = datetime.now()
+    else:
+        # add new entry if kay doesn't exist
+        memcache_stat['num'] += 1
+        memcache[key] = {
+            'file': file,
+            'size': image_size,
+            'timestamp': datetime.now()
+        }
+    # Insert file info to the database(auto-replace previous entry in DB)
+    # update_db_key_list(key, filename, image_size)
+    # Update the size after replacement(not updating 'num' b/c we are just replacing)
+    memcache_stat['size'] += image_size
+    return True
+
+
+def get_memcache(key):
+    """
+    Get the corresponded file content in base64 with a given key in memcache
+
+    :param key: str
+    :return: file: str
+    """
+    if key is None:
+        return None
+
+    if key in memcache.keys():
+        # memcache hit, update statistic and request time
+        update_memcache_stat(missed=False)
+        memcache[key]['timestamp'] = datetime.now()
+        return memcache[key]['file']
+    else:
+        # memcache miss, update statistic
+        update_memcache_stat(missed=True)
+        return None
+
+
+"""
+# Update the memcache entry retrieved from database after a miss
+def update_memcache(key, filename):
+    f_size = get_db_filesize(key)
+    while memcache_stat['size'] + f_size > memcache_config['capacity']:
+        # keep removing entries from memcache until the item can fit into memcache
+        replace_memcache()
+
+    memcache[key] = {
+        'filename': filename,
+        'timestamp': datetime.now()
+    }
+    memcache_stat['num'] += 1
+    memcache_stat['size'] += f_size
+"""
+
+
+# Drop all entries from the memcache
+def clr_memcache():
+    memcache.clear()
+    memcache_stat['size'] = 0
+    memcache_stat['num'] = 0
+    print('memcache is cleared!')
+
+
+# Delete a given key from memcache
+def del_memcache(key):
+    if (key is not None) and (key in memcache.keys()):
+        memcache_stat['size'] -= memcache[key]['size']
+        memcache.pop(key)
+        return True
+    else:
+        print('Error in del_memcache, Key not found in memcache.')
+        return False
+
+
+def send_cloudwatch_response(inst_id, metric_name, metric_value):
+    ts = datetime.utcnow()
+    response = client.put_metric_data(
+            Namespace='MemCache',
+            MetricData=[
+                {
+                    'MetricName': metric_name,
+                    'Dimensions': [
+                        {
+                            'Name': 'InstanceId',
+                            'Value': str(inst_id)
+                        }
+                    ],
+                    'Value': metric_value,
+                    'Timestamp': ts,
+                    'Unit': 'Count',
+                    'StorageResolution': 1  # set to hi-res metric
+                },
+            ]
+        )
+    return response
+
+
+# Called by run.py threading directly
+def store_stats():
+    """Function stores the state of memcache including number of items
+    in cache, total size of items in cache, numbers of requests served,
+    and miss/hit rate.
+    :argument: None
+
+    :return: None
+    """
+    print('Start update memcache status!')
+
+    current_time = datetime.now()
+
+    send_cloudwatch_response(instance_id, 'Items', memcache_stat['num'])
+    send_cloudwatch_response(instance_id, 'Size', memcache_stat['size'])
+    send_cloudwatch_response(instance_id, 'Reqs', memcache_stat['total'])
+    send_cloudwatch_response(instance_id, 'HitRate', memcache_stat['hit_rate'])
+    send_cloudwatch_response(instance_id, 'MissRate', memcache_stat['mis_rate'])
+
+    print('Status Saved! Timestamp is: ', current_time)
---
+++ routes.py
@@ -0,0 +1,269 @@
+from app import managerapp, scheduler, DEBUG, stats, scaler_config, worker_list
+from flask import render_template, request, flash, jsonify, redirect, url_for, json
+from app.stat_data import stats_get_worker_list, stats_aws_get_worker_list
+from app.db_access import update_rds_memcache_config
+from app.ec2_access import ec2_start_instance, ec2_pause_instance
+from random import seed, uniform, randint
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+def send_post_request(addr, data):
+    try:
+        r = requests.post(addr, data=data)
+    except requests.exceptions.RequestException as e:
+        print("Warning: Exception happened when sending the request")
+        if DEBUG is True:
+            print(e)
+
+
+def send_post_request_with_body(addr, params):
+    data = MultipartEncoder(fields=params)
+    headers = {
+        'Content-type': data.content_type
+    }
+    try:
+        r = requests.post(addr, data=data, headers=headers)
+    except requests.exceptions.RequestException as e:
+        print("Warning: Exception happened when sending the request")
+        if DEBUG is True:
+            print(e)
+
+
+def dummy_data():
+    """
+    Function that generate dummy data to test graphs
+    :return:
+    """
+    new_req = randint(0, 2)
+    new_put = randint(0, 2)
+
+    total_item = randint(0, 50)
+    total_size = uniform(5.0, 25.0)
+    total_req = total_item + randint(0, 2)
+    hit_rate = new_req / total_req
+    miss_rate = new_put / total_req
+    print(total_item, total_size, total_req, hit_rate, miss_rate)
+    stats['Workers'].append(randint(1, 8))       # Statistic of number of workers
+    stats['Workers'].pop(0)
+    stats['MissRate'].append(miss_rate)      # Statistic of aggregated Miss Rate
+    stats['MissRate'].pop(0)
+    stats['HitRate'].append(hit_rate)       # Statistic of aggregated Hit Rate
+    stats['HitRate'].pop(0)
+    stats['Items'].append(total_item )        # Statistic of aggregated Total Num of Items
+    stats['Items'].pop(0)
+    stats['Size'].append(total_size)         # Statistic of aggregated Total Size of Contents
+    stats['Size'].pop(0)
+    stats['Reqs'].append(total_req)         # Statistic of aggregated Total Requests
+    stats['Reqs'].pop(0)
+
+
+@managerapp.before_first_request
+def get_stats_tasks():
+    # add the task to scheduler for workers and memcache statistic data updates
+    """scheduler.add_job(id='insert_dummy_data', func=dummy_data, trigger='interval',
+                      seconds=5)"""
+    # add the task to collect worker data, when worker count is greater than 0, another task
+    # that collect the cloudwatch metrics will be added by this function
+    stats_get_worker_list()  # run once at the beginning
+    scheduler.add_job(id='update_worker_count', func=stats_get_worker_list, trigger='interval',
+                      seconds=managerapp.config['JOB_INTERVAL'])
+
+
+@managerapp.route('/')
+@managerapp.route('/stats')
+def stats_display():
+    """
+    The status display of all instances happened here
+    :return:
+    """
+    return render_template('stats_display.html', stats=stats)
+
+
+@managerapp.route('/memcache_config', methods=['GET', 'POST'])
+def memcache_config():
+    """
+    The configuration page of the MemCache rendered here
+    - Included the clear button that clears all the MemCache
+    :return:
+    """
+    if request.method == 'POST':
+        capacity = request.form.get('capacity')
+        rep_policy = request.form.get('rep_policy')
+        if capacity and rep_policy:
+            # update the latest memcache configuration in database
+            update_rds_memcache_config(int(capacity), rep_policy)
+            for instance_id in worker_list.keys():
+                req_addr = 'http://' + managerapp.config['INSTANCE_LIST'][instance_id] + ':5001/refreshconfiguration'
+                send_post_request(req_addr, None)
+            if DEBUG is True:
+                print("New MemCache Setting are: ", capacity, "MB, with ", rep_policy)
+            flash("Configuration Applied!")
+        else:
+            flash("Required Parameter(s) are missing!")
+    return render_template('memcache_config.html')
+
+
+@managerapp.route('/clear')
+def clear_memcache():
+    """
+    The clear commend that clears all the MemCache
+    :return:
+    """
+    for instance_id in worker_list.keys():
+        req_addr = 'http://' + managerapp.config['INSTANCE_LIST'][instance_id] + ':5001/clear'
+        send_post_request(req_addr, None)
+    if DEBUG is True:
+        print('Clear Requests are sent to workers')
+    flash("All MemCache Cleared!")
+    return render_template('memcache_config.html')
+
+
+@managerapp.route('/autoscaler_config', methods=['GET', 'POST'])
+def autoscaler_config():
+    """
+    The configuration page of the AutoScaler rendered here
+    - Included the reset button that delete image data in database and AWS S3
+    :return:
+    """
+    if request.method == 'POST':
+        op_mode_str = request.form.get('op_mode')
+        if op_mode_str == 'Manual':
+            scaler_config['op_mode'] = op_mode_str
+            # Send the mode change to AutoScaler
+            req_addr = managerapp.config['AUTOSCALER_URL']
+            response = {
+                'mode': 'Manual',
+                'add_drop': '0'
+            }
+            send_post_request_with_body(req_addr, response)
+            if DEBUG is True:
+                print('Switching to Manual Mode, Pool Size: ', scaler_config['worker'])
+            flash("Switched to Manual Mode!")
+        elif op_mode_str == 'Automatic':
+            miss_max_str = request.form.get('miss_max')
+            miss_min_str = request.form.get('miss_min')
+            exp_ratio_str = request.form.get('exp_ratio')
+            shr_ratio_str = request.form.get('shr_ratio')
+            if (not miss_max_str) or (not miss_min_str) or (not exp_ratio_str) or (not shr_ratio_str):
+                flash("Required Parameter(s) are missing!")
+            else:
+                miss_max = float(miss_max_str)
+                miss_min = float(miss_min_str)
+                exp_ratio = float(exp_ratio_str)
+                shr_ratio = float(shr_ratio_str)
+                if DEBUG is True:
+                    print('Mode: ', op_mode_str)
+                    print('Miss Rate Max: ', miss_max, 'Miss Rate Min: ', miss_min)
+                    print('Expan Ratio: ', exp_ratio, 'Shrink Ratio ', shr_ratio)
+                scaler_config['op_mode'] = op_mode_str
+                scaler_config['miss_max'] = miss_max
+                scaler_config['miss_min'] = miss_min
+                scaler_config['exp_ratio'] = exp_ratio
+                scaler_config['shr_ratio'] = shr_ratio
+                req_addr = managerapp.config['AUTOSCALER_URL']
+                response = {
+                    'mode': 'Automatic',
+                    'max_miss': miss_max_str,
+                    'min_miss': miss_min_str,
+                    'expand_ratio': exp_ratio_str,
+                    'shrink_ratio': shr_ratio_str
+                }
+                send_post_request_with_body(req_addr, response)
+                flash("Switched to Auto Mode! Applying settings to the AutoScaler")
+        elif DEBUG is True:
+            print('Error: Unknown AutoScaler Operation Mode')
+    return render_template('autoscaler_config.html', config=scaler_config)
+
+
+@managerapp.route('/reset')
+def reset_system():
+    """
+    The reset commend that delete image data in database and AWS S3
+    :return:
+    """
+    req_addr = managerapp.config['FRONTEND_URL'] + 'api/manager/clear'
+    send_post_request(req_addr, None)
+    if DEBUG is True:
+        print('Reset Requests are sent to FrontEndApp')
+    flash("All Application Data are Deleted!")
+    return render_template('autoscaler_config.html', config=scaler_config)
+
+
+@managerapp.route('/start_worker', methods=['GET', 'POST'])
+def start_worker():
+    """
+    Start a worker in manual mode
+    :return:
+    """
+    if scaler_config['op_mode'] != 'Manual':
+        scaler_config['op_mode'] = 'Manual'
+    # get the current worker lists
+    stopped_worker = stats_aws_get_worker_list('stopped')
+    pending_worker = stats_aws_get_worker_list('pending')
+    if DEBUG is True:
+        print('There are', len(stopped_worker), 'running workers, and ', len(pending_worker))
+
+    if pending_worker:
+        flash("A worker status is pending, please try again later")
+    elif not stopped_worker:
+        flash("No free worker is available at the moment.")
+    else:
+        if scaler_config['worker'] < 8:
+            scaler_config['worker'] += 1
+            # Send the new configuration to AutoScaler
+            req_addr = managerapp.config['AUTOSCALER_URL']
+            response = {
+                'mode': 'Manual',
+                'add_drop': '1'
+            }
+            send_post_request_with_body(req_addr, response)
+            # NOTE: Manual start a EC2 instance is handled by AutoScaler now
+            # ec2_start_instance(stopped_worker[0])
+            flash("Switched to Manual Mode. Please waiting for worker to boot up.")
+        else:
+            flash("Maximum Worker is Running!")
+
+        if DEBUG is True:
+            print('Switching to Manual Mode, Pool Size: ', scaler_config['worker'])
+    return render_template('autoscaler_config.html', config=scaler_config)
+
+
+@managerapp.route('/pause_worker', methods=['GET', 'POST'])
+def pause_worker():
+    """
+    Pause a worker in manual mode
+    :return:
+    """
+    # get the current worker lists
+    running_worker = stats_aws_get_worker_list('running')
+    pending_worker = stats_aws_get_worker_list('pending | starting | stopping | shutting-down')
+    if DEBUG is True:
+        print('There are', len(running_worker), 'running workers, and ', len(pending_worker))
+
+    if pending_worker:
+        flash("A worker status is pending, please try again later")
+    elif not running_worker:
+        flash("All worker is stopped at the moment.")
+    else:
+        if scaler_config['op_mode'] != 'Manual':
+            scaler_config['op_mode'] = 'Manual'
+        if scaler_config['worker'] > 1:
+            scaler_config['worker'] -= 1
+            # Send the new configuration to AutoScaler
+            req_addr = managerapp.config['AUTOSCALER_URL']
+            response = {
+                'mode': 'Manual',
+                'add_drop': '-1'
+            }
+            send_post_request_with_body(req_addr, response)
+            # NOTE: Manual start a EC2 instance is handled by AutoScaler now
+            # ec2_pause_instance(running_worker[-1])
+            flash("Switched to Manual Mode. Please waiting for worker to stop.")
+        else:
+            flash("At least one worker is required to running.")
+        if DEBUG is True:
+            print('Switching to Manual Mode, Pool Size: ', scaler_config['worker'])
+
+    return render_template('autoscaler_config.html', config=scaler_config)
+
